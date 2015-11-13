b1f31f6398e4ed6b53710d896bdefc3f4590f814
adesanto <aarondesanto@gmail.com>
2015-11-12 19:34:56 -0500

    words argument is now optional.  general cleanup.

--- contentment.js
+++ contentment.js
@@ -3,33 +3,33 @@
 	$.fn.contentment = function(chars, words) {
 		var $characters = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',' '];
 		
-		function $randomMaths(maxL, minL) {
+		function $mathRand(maxL, minL) {
 			return Math.floor((Math.random() * maxL) + parseInt(minL)); // Generates a random number between minL and maxL
 		};
 		
-		if (words !== null) {
+		if (words !== undefined) {
 		
-			for (var a = 0; a < parseInt(words); a++) {
+			for (var w = 0; w < parseInt(words); w++) {
 			
 				var $theWord = [];
 				
-				for (var k = 0; k < parseInt(chars); k++) {
+				for (var c = 0; c < parseInt(chars); c++) {
 				
-					$theWord.push($characters[$randomMaths(25, 1)]);
+					$theWord.push($characters[$mathRand(25, 1)]);
 				}
 				
 				$theWord = $theWord.toString().replace(/\,/gi, "");
-				this.append(" " + $theWord);
+				this.append($theWord + " ");
 				
 			}
 			
-		} else if (words === null) {
-		
-			for (var i = 0; i <= parseInt(chars); i++) {
-				this.append($characters[$randomMaths(26, 1)]);
-			}
-			
 		}
+		
+		for (var i = 0; i <= parseInt(chars); i++) {
+			this.append($characters[$mathRand(26, 1)]);
+		}
+		
+		words = words || null;
 		
 		return this;
 	};
--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
 
-	$('.someDiv').contentment(10, 20);
+	$('.someDiv').contentment(6);
 
 });
