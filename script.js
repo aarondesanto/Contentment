398f8ae88f3539bd2e37fac04c74906fca82b13b
adesanto <aarondesanto@gmail.com>
2015-11-14 13:11:54 -0500

    example site included

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 $(document).ready(function() {
 
 	$('.titles').contentment(10, 3);
-	$('.content').contentment(5, 100);
-	$('.caption').contentment(3, 3);
+	$('.content').contentment(5, 500);
+	$('.nav').contentment(5, 4);
+	$('.navList').contentment(2, 1);
+	$('#navOne').contentment(2, 2);
+	$('#navTwo').contentment(3, 3);
+	$('#navThree').contentment(4, 2);
+	$('#navFour').contentment(3, 2);
 
 });
